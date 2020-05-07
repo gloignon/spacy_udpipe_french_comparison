3009ea3fb974445fde4db3c6660b0b344ed5a787
gloignon <guillaume.loignon@gmail.com>
2020-05-07 10:39:41 -0400

    fixed bug in model selection

--- overall_graph.R
+++ overall_graph.R
@@ -1,12 +1,12 @@
 library(data.table)
-x <- readLines("README.md")
+#x <- readLines("README.md")
 x <- grep("^Tokens|^Sentences|^Words|^UPOS|^XPOS|^Feats|^AllTags|^Lemmas|^UAS|^LAS|^CLAS", x, value=TRUE)
 x <- lapply(x, FUN=function(x) data.frame(t(trimws(strsplit(x, "\\|")[[1]]))))
 x <- rbindlist(x, fill = TRUE)
 colnames(x) <- c("metric", "precision", "recall", "f1", "alignedacc")
-x$treebank <- unlist(lapply(c("French Sequioa", "Dutch", "Spanish-Ancora", "Portuguese", "Italian", "English"),
+x$treebank <- unlist(lapply(c("French Sequioa"),
                      FUN=function(x) rep(x, 11*2)))
-x$model <- unlist(lapply(c("French Sequioa", "Dutch", "Spanish-Ancora", "Portuguese", "Italian", "English"),
+x$model <- unlist(lapply(c("French Sequioa"),
                             FUN=function(x) c(rep("UDPipe", 11), rep("spaCy", 11))))
 x$precision <- as.numeric(as.character(x$precision))
 x$recall <- as.numeric(as.character(x$recall))
@@ -17,10 +17,6 @@ library(lattice)
 x$treebank <- factor(x$treebank)
 x <- x[order(x$treebank), ]
 x <- subset(x, !(treebank %in% "French Sequioa" & metric == "XPOS"))
-x <- subset(x, !(treebank %in% "Portuguese" & metric == "Feats"))
-x <- subset(x, !(treebank %in% "Portuguese" & metric == "XPOS"))
-x <- subset(x, !(treebank %in% "English" & metric == "Feats"))
-x <- subset(x, !(treebank %in% "English" & metric == "UPOS"))
 #x <- subset(x, treebank != "English")
 trellis.par.set(name = "strip.background", value = list(col = "honeydew2"))
 xyplot(alignedacc ~ treebank | metric, groups = model, 
--- udpipe-spacy-comparison.R
+++ udpipe-spacy-comparison.R
@@ -8,14 +8,17 @@ library(data.table)
 ##  + udpipe + spacy were trained on the same data
 ##  + Note: no xpos in this treebank
 ##############################################################################################
+
+## Uncomment and run the following to grab a fresh copy of the test data.
 #download.file(url = "https://raw.githubusercontent.com/UniversalDependencies/UD_French-Sequoia/master/fr_sequoia-ud-test.conllu", 
-#              destfile = "gold.conllu")
+#             destfile = "gold.conllu")
+
+
+# Uncomment and run following instead for old version of test data
 #download.file(url = "https://github.com/UniversalDependencies/UD_French-Sequoia/archive/r2.0-test.zip",
 #              destfile = "r2.0-test.zip")
-#unzip("r2.0-test.zip", list = TRUE)
-#unzip("r2.0-test.zip", files = "UD_French-Sequoia-r2.0-test/fr_sequoia-ud-test.conllu", exdir = getwd(), junkpaths = TRUE)
+#unzip("r2.5-test.zip", files = "UD_French-Sequoia-r2.0-test/fr_sequoia-ud-test.conllu", exdir = getwd(), junkpaths = TRUE)
 #file.copy("fr_sequoia-ud-test.conllu", "gold.conllu", overwrite = TRUE)
-
 
 gold <- udpipe_read_conllu("gold.conllu")
 sentences <- unique(gold$sentence)
@@ -25,18 +28,20 @@ sentences <- grep(pattern = "# text =", sentences, value=TRUE)
 sentences <- gsub("# text = ", "", sentences)
 
 ## Annotation with UDPipe GSD
-load_model_french <-
+model_GSD <-
   udpipe_load_model(file = "french-gsd-ud-2.4-190531.udpipe") #chargement du modèle linguistique
-anno <- udpipe_annotate(udmodel_french, sentences)
-cat(anno$conllu, file = file("predictions_udpipe.conllu", encoding = "UTF-8"))
-x <- as.data.frame(anno)
-
+anno_gsd <- udpipe_annotate(model_GSD, sentences)
+cat(anno_gsd$conllu, file = file("predictions_udpipe_gsd.conllu", encoding = "UTF-8"))
+dt.gsd <- setDT(copy(as.data.frame(anno_gsd)))
 
 ## Annotation with UDPipe Sequoia
-load_model <- udpipe_load_model("french-sequoia-ud-2.1-20180111.udpipe")
-anno <- udpipe_annotate(udmodel_french, sentences)
-cat(anno$conllu, file = file("predictions_udpipe.conllu", encoding = "UTF-8"))
-x <- as.data.frame(anno)
+model_sequoia <- udpipe_load_model("french-sequoia-ud-2.4-190531.udpipe")
+anno_sequoia <- udpipe_annotate(model_sequoia, sentences)
+cat(anno_sequoia$conllu, file = file("predictions_udpipe_sequoia.conllu", encoding = "UTF-8"))
+dt.sequoia <- setDT(copy(as.data.frame(anno_sequoia)))
+
+## Simple comparison of GSD and Sequoia's lemmatization
+setdiff(dt.gsd$lemma, dt.sequoia$lemma)
 
 ## Annotation with Spacy
 #spacy_finalize()
@@ -54,5 +59,6 @@ cat(as_conllu(x), file = file("predictions_spacy.conllu", encoding = "UTF-8"))
 spacy_finalize()
 
 ## Compare evaluations
-system("python evaluation_script/conll17_ud_eval.py -v gold.conllu predictions_udpipe.conllu")
+system("python evaluation_script/conll17_ud_eval.py -v gold.conllu predictions_udpipe_gsd.conllu")
+system("python evaluation_script/conll17_ud_eval.py -v gold.conllu predictions_udpipe_sequoia.conllu")
 system("python evaluation_script/conll17_ud_eval.py -v gold.conllu predictions_spacy.conllu")
